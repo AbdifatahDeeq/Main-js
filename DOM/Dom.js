a36c431de06b2377895b5aaeaaa17e8e140aade6
AbdifatahDeeq <deeq8763@gmail.com>
2025-07-18 00:58:23 +0200

    Exercise #32

---
+++ DOM/Dom.js
@@ -0,0 +1,12 @@
+// const header = document.getElementById("head")
+// console.log(header)
+
+// const txt = document.getElementsByClassName("text");
+// console.log(txt)
+
+//Exercise #32 querySelector and querySelectorAll
+
+const header = document.querySelector("#head")
+console.log(header)
+const text = document.querySelectorAll(".text")
+console.log(text)
