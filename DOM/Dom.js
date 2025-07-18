f59fc42eb03761d7df372cbc9236475d7cf11a2e
AbdifatahDeeq <deeq8763@gmail.com>
2025-07-18 10:31:58 +0200

    Exercise #33

--- DOM/Dom.js
+++ DOM/Dom.js
@@ -6,7 +6,15 @@
 
 //Exercise #32 querySelector and querySelectorAll
 
-const header = document.querySelector("#head")
-console.log(header)
-const text = document.querySelectorAll(".text")
-console.log(text)
+// const header = document.querySelector("#head")
+// console.log(header)
+// const text = document.querySelectorAll(".text")
+// console.log(text)
+
+
+//Exercise #33
+function change() {
+  head.textContent = "Welcome to the DOM";
+
+}
+function changeContent() { subject.innerHTML = "PHYTHONE"; }
