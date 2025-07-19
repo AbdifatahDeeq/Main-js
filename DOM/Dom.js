23bf0cf8b7e1c0e3fcb9170881d82fdafc88c221
AbdifatahDeeq <deeq8763@gmail.com>
2025-07-19 12:45:06 +0200

    Exercise #34

--- DOM/Dom.js
+++ DOM/Dom.js
@@ -13,8 +13,38 @@
 
 
 //Exercise #33
-function change() {
-  head.textContent = "Welcome to the DOM";
+// function change() {
+//   head.textContent = "Welcome to the DOM";
 
+// }
+// function changeContent() { subject.innerHTML = "PHYTHONE"; }
+
+//Add and removing items 
+
+// function addItem() {
+//   const list = document.querySelector("#list");
+//   const newItem = document.createElement("li");
+//   newItem.textContent = "item 5";
+//   list.appendChild(newItem);
+// }
+
+//Exercise #34
+const list = document.querySelector("#list")
+
+function addItem() {
+  const newItem = document.createElement("li")
+  newItem.textContent = "Item5"
+  list.appendChild(newItem);
+    
 }
-function changeContent() { subject.innerHTML = "PHYTHONE"; }
+  
+
+function remove() {
+  if (list.lastChild) {
+     list.removeChild(list.lastChild);
+  }
+  else {
+alert(" It is End")
+ }
+  
+}
